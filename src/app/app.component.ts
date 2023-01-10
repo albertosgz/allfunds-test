2f01ee5cbc474a6d798344bb910b75872674ab6b
Alberto Sanchez Gonzalez <alberto.mu.es@gmail.com>
2023-01-10 16:34:45 +0100

    Initial Commit
    Test working as expected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ApiService, QuoteData } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'allfunds-test';
+
+  private subscriptions$: Subscription[] = [];
+
+  quote: QuoteData = {
+    Last: {},
+    Close: {},
+    DayChangePerc: {},
+    DayChange: {},
+    Volume: {},
+    Turnover: {},
+    PreviousYearClose: {},
+    YTD: {},
+  };
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.subscriptions$.push(
+      this.apiService.getQuote().subscribe(quote => {
+        this.quote = quote;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach(sub$ => sub$.unsubscribe());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ChangingValueComponent } from './changing-value/changing-value.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ChangingValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
